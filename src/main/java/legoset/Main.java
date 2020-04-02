dbb2a8592b9bdfe649f0d0a697767a065d80296d
Máté Stiegelmayer <stigimate@gmail.com>
2020-04-02 18:53:23 +0200

    Package called 'legoset' added

---
+++ src/main/java/legoset/Main.java
@@ -0,0 +1,35 @@
+package legoset;
+
+import java.util.ArrayList;
+import java.time.Year;
+import java.util.Set;
+
+import jaxb.JAXBHelper;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        LegoSet legoset = new LegoSet();
+
+        legoset.setNumber("75211");
+        legoset.setName("Imperial TIE Fighter");
+        legoset.setTheme("Star Wars");
+        legoset.setSubtheme("Solo");
+        legoset.setYear(Year.of(2018));
+        legoset.setPieces(519);
+        legoset.setTags(Set.of("Starfighter", "Stormtrooper", "Star Wars", "Solo"));
+
+        ArrayList<Minifig> minifigs = new ArrayList<>();
+        minifigs.add(new Minifig(2, "Imperial Mudtropper"));
+        minifigs.add(new Minifig(1, "Imperial Pilot"));
+        minifigs.add(new Minifig(1, "Mimban Stormtrooper"));
+        legoset.setMinifigs(minifigs);
+
+        legoset.setWeight(new Weight("kg", 0.89));
+        legoset.setUrl("https://brickset.com/sets/75211-1/Imperial-TIE-Fighter");
+
+        JAXBHelper.toXML(legoset, System.out);
+
+
+    }
+}
